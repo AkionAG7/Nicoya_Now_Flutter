bff8ee561b94d127524954e4f51929ff5fea9235
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2025-05-24 03:08:49 -0600

    feat/ cambios

--- lib/app/features/merchant/domain/usecases/register_merchant_usecase.dart
+++ lib/app/features/merchant/domain/usecases/register_merchant_usecase.dart
@@ -1,4 +1,3 @@
-import 'package:image_picker/image_picker.dart';
 import 'package:nicoya_now/app/features/merchant/domain/entities/merchant.dart';
 import 'package:nicoya_now/app/features/merchant/domain/repositories/merchant_repository.dart';
 
