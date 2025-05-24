dbae1b98fd31094b8421aec02a64f2da2288062f
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-05-24 16:50:29 -0600

    crear pagina de ver mas

---
+++ lib/app/interface/Widgets/FoodFilter.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FoodFilter extends StatefulWidget {
+  const FoodFilter({ Key? key }) : super(key: key);
+
+  @override
+  _FoodFilterState createState() => _FoodFilterState();
+}
+
+class _FoodFilterState extends State<FoodFilter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
