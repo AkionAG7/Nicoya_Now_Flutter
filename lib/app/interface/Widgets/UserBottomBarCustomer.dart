c6de50ea7d6d28ff7d8a4363166d630933095289
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-05-30 23:37:27 -0600

    pantalla de usuario de customer

---
+++ lib/app/interface/Widgets/UserBottomBarCustomer.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:nicoya_now/app/features/auth/data/datasources/auth_data_source.dart';
+import 'package:nicoya_now/app/interface/Navigators/routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class UserBottomBarCustomer extends StatefulWidget {
+  const UserBottomBarCustomer({Key? key}) : super(key: key);
+
+  @override
+  _UserBottomBarCustomerState createState() => _UserBottomBarCustomerState();
+}
+
+class _UserBottomBarCustomerState extends State<UserBottomBarCustomer> {
+  final AuthDataSource authDataSource = SupabaseAuthDataSource(
+    Supabase.instance.client,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 40, right: 40),
+            child: SizedBox(
+              width: double.infinity,
+              height: 70,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFf10027),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () async {
+                  await authDataSource.signOut();
+                  if (!mounted) return;
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    Routes.login_page,
+                    (route) => false,
+                  );
+                },
+                child: Text(
+                  'Cerrar Sesión',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
